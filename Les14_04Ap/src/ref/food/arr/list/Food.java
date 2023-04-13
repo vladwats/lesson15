12def53d9b1f55012441cea3fda8e807b4f15cf5
vladwats <106876485+vladwats@users.noreply.github.com>
2023-04-13 15:40:45 +0300

    Update Food.java

--- Les14_04Ap/src/ref/food/arr/list/Food.java
+++ Les14_04Ap/src/ref/food/arr/list/Food.java
@@ -3,32 +3,25 @@ package ref.food.arr.list;
 import java.util.Objects;
 
 public class Food {
-	
-	// private int number;  // порядковый номер продукта в холодильнике
-	private String type;  // вид (тип) продукта питания 
-	private String brand; // торговая марка продукта питания 
+
+	private String type; // вид (тип) продукта питания
+	private String brand; // торговая марка продукта питания
 	private String chamber; // холодильная камера (cooling) или морозильная (freezer)
-	private int shelf_number; // номер полки, отсчет сверху вниз (где 1-я полка - самая верхняя);
-	                          // для холодильной камеры и морозильной отсчет полок раздельный (1-я полка холод. камеры; 1-я- морозильной
-	                          // боковых полок и выдвижных ящиков в холодильной камере в холодильнике нет
-	
-	
-public Food() {}
-	
-	public Food(String type, String brand, String chamber, int shelf_number) {
+	private int shelf; // номер полки, отсчет сверху вниз (где 1-я полка - самая верхняя);
+						// для холодильной камеры и морозильной отсчет полок раздельный (1-я полка
+						// холод. камеры; 1-я- морозильной)
+						// боковых полок и выдвижных ящиков в холодильной камере в холодильнике нет
+
+	public Food() {
+	}
+
+	public Food(String type, String brand, String chamber, int shelf) {
 		super();
-		//this.number = number;
 		this.type = type;
 		this.brand = brand;
 		this.chamber = chamber;
-		this.shelf_number = shelf_number;			
+		this.shelf = shelf;
 	}
-
-	/*
-	 * public int getNumber() { return number; }
-	 * 
-	 * public void setNumber(int number) { this.number = number; }
-	 */
 
 	public String getType() {
 		return type;
@@ -54,17 +47,17 @@ public Food() {}
 		this.chamber = chamber;
 	}
 
-	public int getShelf_number() {
-		return shelf_number;
+	public int getShelf() {
+		return shelf;
 	}
 
-	public void setShelf_number(int shelf_number) {
-		this.shelf_number = shelf_number;
+	public void setShelf_number(int shelf) {
+		this.shelf = shelf;
 	}
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(brand, chamber, shelf_number, type);
+		return Objects.hash(brand, chamber, shelf, type);
 	}
 
 	@Override
@@ -76,13 +69,9 @@ public Food() {}
 		if (getClass() != obj.getClass())
 			return false;
 		Food other = (Food) obj;
-		return Objects.equals(brand, other.brand) && Objects.equals(chamber, other.chamber)
-				&& shelf_number == other.shelf_number && Objects.equals(type, other.type);
+		return Objects.equals(brand, other.brand) && Objects.equals(chamber, other.chamber) && shelf == other.shelf
+				&& Objects.equals(type, other.type);
 	}
-	
-	
-	
-	
-	
 
 }
+
